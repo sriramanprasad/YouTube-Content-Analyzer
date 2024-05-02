3ea570aa0d33238c9e6a7f16ada59f7352e93562
Sri Raman Prasad <91125096+sriramanprasad@users.noreply.github.com>
2024-05-02 20:20:14 +0530

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -8,7 +8,7 @@ from functools import lru_cache
 
 
 # Initialize AssemblyAI
-aai.settings.api_key = st.secrets["c0ea59072d71406aae8d27ada72aced4"]
+aai.settings.api_key = st.secrets["1234"]
 
 # Set the page width to a custom value
 st.set_page_config(layout="wide")
